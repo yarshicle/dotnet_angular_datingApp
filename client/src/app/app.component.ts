e48db09c9ad3b8440e419bdea6d9fb17e9b27c12
Josh Smith <joshsmithdev@gmail.com>
2021-01-14 15:22:33 -0700

    End of Section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
 	selector: 'app-root',
@@ -12,17 +14,14 @@ export class AppComponent implements OnInit {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	users: any;
 
-	constructor(private http: HttpClient) {}
+	constructor(private accountService: AccountService) {}
 	
 	ngOnInit() {
-		this.getUsers();
+		this.setCurrentUser();
 	}
 
-	getUsers() {
-		this.http.get('https://localhost:5001/api/users').subscribe(response => {
-			this.users = response;
-		}, error => {
-			console.log(error);
-		});
+	setCurrentUser() {
+		const user: User = JSON.parse(localStorage.getItem('user'));
+		this.accountService.setCurrentUser(user);
 	}
 }
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,30 @@
+import { AccountService } from './../_services/account.service';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+	@Output() cancelRegister = new EventEmitter();
+	model: any = {};
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+	register() {
+		this.accountService.register(this.model).subscribe(response => {
+			console.log(response);
+			this.cancel();
+		}, err => {
+			console.log(err);
+		});
+	}
+
+	cancel() {
+		this.cancelRegister.emit(false);
+	}
+}
