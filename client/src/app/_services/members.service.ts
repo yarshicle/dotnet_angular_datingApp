22f43f8181d1e633d86cc405017e28361d4521e4
Josh Smith <joshsmithdev@gmail.com>
2021-01-16 21:47:25 -0700

    End of Section 9

---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,21 @@
+import { environment } from './../../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Member } from '../models/member';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+	baseUrl = environment.apiUrl;
+
+	constructor(private http: HttpClient) { }
+	
+	getMembers() {
+		return this.http.get<Member[]>(this.baseUrl + 'users');
+	}
+
+	getMember(username: string) {
+		return this.http.get<Member>(this.baseUrl + 'users/' + username);
+	}
+}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { SharedModule } from './_modules/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
@@ -18,6 +19,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +33,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
     MessagesComponent,
     TestErrorsComponent,
     NotFoundComponent,
-    ServerErrorComponent
+    ServerErrorComponent,
+    MemberCardComponent
   ],
   imports: [
     BrowserModule,
@@ -42,7 +45,8 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
 		SharedModule
   ],
   providers: [
-		{provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+		{provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+		{provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true}
 	],
   bootstrap: [AppComponent]
 })
