5821ce9d2c1c688412656f52fafc0a07ed44a9b4
Josh Smith <joshsmithdev@gmail.com>
2021-01-19 11:54:51 -0700

    End of Section 12

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,7 +1,8 @@
+import { Router } from '@angular/router';
 import { AccountService } from './../_services/account.service';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
-import { error } from '@angular/compiler/src/util';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 
 @Component({
@@ -11,19 +12,43 @@ import { error } from '@angular/compiler/src/util';
 })
 export class RegisterComponent implements OnInit {
 	@Output() cancelRegister = new EventEmitter();
-	model: any = {};
-  constructor(private accountService: AccountService, private toastr: ToastrService) { }
+	registerForm: FormGroup;
+	maxDate: Date;
+	validationErrors: string[] = [];
+
+  constructor(private accountService: AccountService, private toastr: ToastrService, private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
-  }
+		this.initializeForm();
+		this.maxDate = new Date();
+		this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+	}
+	
+	initializeForm() {
+		this.registerForm = this.fb.group({
+			gender: ['male'],
+			username: ['', Validators.required],
+			knownAs: ['', Validators.required],
+			dateOfBirth: ['', Validators.required],
+			city: ['', Validators.required],
+			country: ['', Validators.required],
+			password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(10)]],
+			confirmPassword: ['', [Validators.required, this.matchValues('password')]]
+
+		})
+	}
+
+	matchValues(matchTo: string): ValidatorFn {
+		return(control: AbstractControl) => {
+			return control?.value === control?.parent?.controls[matchTo].value ? null : {isMatching: true};
+		}
+	}
 
 	register() {
-		this.accountService.register(this.model).subscribe(response => {
-			console.log(response);
-			this.cancel();
-		}, err => {
-			console.log(err);
-			this.toastr.error(err.error);
+		this.accountService.register(this.registerForm.value).subscribe(response => {
+			this.router.navigateByUrl('/members');
+		}, error => {
+			this.validationErrors = error;
 		});
 	}
 
