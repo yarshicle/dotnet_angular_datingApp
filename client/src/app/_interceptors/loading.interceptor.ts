695c2894258ace664180c9a395a9b12f7e8b572b
Josh Smith <joshsmithdev@gmail.com>
2021-02-05 20:27:07 -0700

    1 more change...

--- client/src/app/_interceptors/loading.interceptor.ts
+++ client/src/app/_interceptors/loading.interceptor.ts
@@ -19,7 +19,6 @@ export class LoadingInterceptor implements HttpInterceptor {
 	): Observable<HttpEvent<unknown>> {
 		this.busyService.busy();
 		return next.handle(request).pipe(
-			delay(0),
 			finalize(() => {
 				this.busyService.idle();
 			})
